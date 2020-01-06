8ebc42b36da44fc3fb292e5f03805fe4f1ee7899
Mark Brazeau <kitsapbackflow@gmail.com>
2020-01-06 23:33:21 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,10 +48,12 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  
+  totallity = 0
   index = 0 
-   #while index < source.length do
+   while index < source.length do
       total = directors_totals(source)
+      gross = total[index[]]
+ 
   binding.pry
   
   
